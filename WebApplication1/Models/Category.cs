7a3335e038702662326d1e679d8872aa89fbd448
mitchellmisayah <mitchellmisayah@gmail.com>
2023-04-28 03:43:25 -0500

    Input Tag Helpers

--- WebApplication1/Models/Category.cs
+++ WebApplication1/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
@@ -7,7 +8,9 @@ namespace WebApplication1.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
