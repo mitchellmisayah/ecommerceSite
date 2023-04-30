6d3243871710c1deeb97dae9f30693aa28adcb82
mitchellmisayah <mitchellmisayah@gmail.com>
2023-04-30 09:23:07 -0500

    Get Category Details to Edit

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -17,7 +17,7 @@ namespace WebApplication1.Controllers
             var objCategoryList = this.db.Categories.ToList(); //List<Category> objCategoryList = this.db.Categories.ToList();  they're the same
             return View(objCategoryList);
         }
-
+        //Create
         public IActionResult Create()
         {
             return View();
@@ -25,6 +25,46 @@ namespace WebApplication1.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                this.db.Categories.Add(obj);
+                this.db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //Edit
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            
+            Category? categoryFromDb = this.db.Categories.Find(id); //finds the primary key of Category and assigns it to categoryFromDb
+
+            //Other ways that work
+            //Category? categoryFromDb1 = this.db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Category? categoryFromDb2 = this.db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
