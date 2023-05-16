86fa9c0fa87e70e5c725f2493fa791478595b5ad
mitchellmisayah <mitchellmisayah@gmail.com>
2023-05-16 00:55:25 -0500

    94.Create Product Image upload

--- WebApplication1/Areas/Admin/Controllers/ProductController.cs
+++ WebApplication1/Areas/Admin/Controllers/ProductController.cs
@@ -13,11 +13,13 @@ namespace WebApplication1.Areas.Admin.Controllers
     {
         
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -66,6 +68,19 @@ namespace WebApplication1.Areas.Admin.Controllers
 
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file!= null)
+                {
+                    string fileName = Guid.NewGuid().ToString()+ Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product" + fileName;
+                }
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
